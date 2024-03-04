1b3be9d742f071ae6de0b9d893c00b8df2f6cf74
Rama Sai <ramasai.deva@gmail.com>
2024-03-04 20:03:56 +0530

    sum of n numbers
    here input string can be "1", "1,2", "3,4,5,..upto n numbers"

--- calculator.rb
+++ calculator.rb
@@ -3,6 +3,15 @@ class Calculator
 
     def add(input_str)
         return 0 if input_str.empty?
+
+        # Taking comma(,) as default delimiter.
+        delimiter = ','
+
+        # returns the array of integers
+        num_array = input_str.split(delimiter).map(&:to_i)
+
+        # calculating sum of interges using array built-in method(sum)
+        num_array.sum
     end
  
 end
--- calculator_spec.rb
+++ calculator_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe Calculator do
       it "returns 0 for an empty string" do
         expect(calculator.add("")).to eq(0)
       end
+
+      it "returns the number for a single number string" do
+        expect(calculator.add("1")).to eq(1)
+      end
+  
+      it "returns the sum of two numbers" do
+        expect(calculator.add("1,5")).to eq(6)
+      end
     end
 end
